3f381aeeb0e8f7caaab56c6bbd0adc57fab4ce29
Igor Rautiainen <igor.rautiainen@outlook.com>
2022-07-19 19:12:54 +0300

    Receiving locations from Accuweather

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import { useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
-  const link = "http://www.google.com";
-  const [state, setState] = useState(1);
+  // const link = "http://www.google.com";
+  // const [state, setState] = useState(1);
+  const [citySearch, setCitySearch] =useState('');
+  const [cityKey, setCityKey] = useState(null);
+
+  const fetchCity=(e)=> {
+    e.preventDefault();
+    axios.get(`http://dataservice.accuweather.com/locations/v1/cities/search?apikey=8LYjRmrxFnGAGVTSE4qQnc7XxhnO45t9&q=${citySearch}`)
+    .then ((response)=>{
+      setCitySearch('');
+      setCityKey(response.data[0])
+      console.log(response.data)});
+  }
+
   return (
     <div className="App">
         <h2>Integrify assignment July 2022 - Accuweather</h2>
-        <form>
+        <form onSubmit={fetchCity}>
           <label>Enter the city name</label> 
-          <input required placeholder='City name'></input>
+          <input required placeholder='City name' value={citySearch} onChange={(e)=>setCitySearch(e.target.value)}></input>
           <button type='submit'>Submit</button>
         </form>
-        <section>
+
+
+        {/* <section>
           <div>{state}</div>
           <button onClick={() => setState(state - 1)}>Less</button>
           <button onClick={() => setState(state + 1)}>More</button>
@@ -23,7 +38,7 @@ function App() {
         <p>test</p>
         <p> {link}</p>
         <a href={link}> Google </a>
-      </div>
+      </div> */}
     </div>
   );
 }
