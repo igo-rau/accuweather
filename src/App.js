588b931a9730a1b1692c1b8241a6b9deb6e3730e
Igor Rautiainen <igor.rautiainen@outlook.com>
2022-07-19 18:46:58 +0300

    Basic form plus some React tests

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const link = "http://www.google.com";
+  const [state, setState] = useState(1);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h2>Integrify assignment July 2022 - Accuweather</h2>
+        <form>
+          <label>Enter the city name</label> 
+          <input required placeholder='City name'></input>
+          <button type='submit'>Submit</button>
+        </form>
+        <section>
+          <div>{state}</div>
+          <button onClick={() => setState(state - 1)}>Less</button>
+          <button onClick={() => setState(state + 1)}>More</button>
+        </section>
+
+
+      <div className="content">
+        <p>test</p>
+        <p> {link}</p>
+        <a href={link}> Google </a>
+      </div>
     </div>
   );
 }
 
 export default App;
+//8LYjRmrxFnGAGVTSE4qQnc7XxhnO45t9
