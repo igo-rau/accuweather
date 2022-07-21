3be2553d885d42fb571ffdc87017cdd3600da3ab
Igor Rautiainen <igor.rautiainen@outlook.com>
2022-07-21 17:53:21 +0300

    Cosmetical changes, html tags checked.

--- src/App.js
+++ src/App.js
@@ -1,40 +1,55 @@
-import { useState } from 'react';
-import './App.css';
-import axios from 'axios';
-import Widget from './Widget';
+import { useState } from "react";
+import axios from "axios";
+
+import Widget from "./Widget";
+import "./App.css";
 
 function App() {
-
-  const [citySearch, setCitySearch] =useState('');
+  const [citySearch, setCitySearch] = useState("");
   const [cityInfo, setCityInfo] = useState(null);
 
-  const fetchCity=(e)=> {
+  const fetchCity = (e) => {
     e.preventDefault();
-    axios.get(`https://dataservice.accuweather.com/locations/v1/cities/search?apikey=8LYjRmrxFnGAGVTSE4qQnc7XxhnO45t9&q=${citySearch}`)
-    .then ((response)=>{
-      setCitySearch('');
-      setCityInfo(response.data[0]);
-      console.log(response.data)});
-      console.log("fetchcity called");
-  }
+    axios
+      .get(
+        `https://dataservice.accuweather.com/locations/v1/cities/search?apikey=8LYjRmrxFnGAGVTSE4qQnc7XxhnO45t9&q=${citySearch}`
+      )
+      .then((response) => {
+        setCitySearch("");
+        setCityInfo(response.data[0]);
+      });
+  };
 
   return (
-    <div className="App">
-        <h2>Integrify assignment July 2022 - Accuweather</h2>
+    <>
+    <header>
+    </header>
+    <main>
+      <div className="App">
+        <h1>Integrify assignment July 2022 - Accuweather</h1>
         <form onSubmit={fetchCity}>
-          <label>Enter the city name</label> 
-          <input required placeholder='City name' value={citySearch} onChange={(e)=>setCitySearch(e.target.value)}></input>
-          <button type='submit'>Submit</button>
+          <label htmlFor="cityInput">Enter the city name</label>
+          <input
+            required
+            placeholder="City name"
+            name="cityInput"
+            value={citySearch}
+            onChange={(e) => setCitySearch(e.target.value)}
+          ></input>
+          <button type="submit">Submit</button>
         </form>
         {/* Add "not found" option */}
-        {cityInfo&&(
+        {cityInfo && (
           <div>
-            <Widget cityInfo={cityInfo}/>
+            <Widget cityInfo={cityInfo} />
           </div>
         )}
-        
-
-    </div>
+      </div>
+    </main>
+    <footer>
+      Copyright &copy; Igor Rautiainen <a href="https://github.com/igo-rau/accuweather">GitHub</a>
+    </footer> 
+    </>
   );
 }
 
--- src/Widget.js
+++ src/Widget.js
@@ -1,41 +1,43 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
+import axios from "axios";
+import { useEffect, useState } from "react";
 
+const Widget = ({ cityInfo }) => {
+  const [conditions, setConditions] = useState(null);
 
-const Widget = ({cityInfo}) => {
-    
-    const [conditions, setConditions]=useState(null);
-    
-    useEffect(() => {
-        // console.log("Widget called. Cityinfo:");
-        // console.log(cityInfo);
-        axios.get(`https://dataservice.accuweather.com/currentconditions/v1/${cityInfo.Key}?apikey=8LYjRmrxFnGAGVTSE4qQnc7XxhnO45t9`)
-        .then((response)=>{
-            setConditions(response.data[0]);
-        })
-    }, [cityInfo])
+  useEffect(() => {
+    axios
+      .get(
+        `https://dataservice.accuweather.com/currentconditions/v1/${cityInfo.Key}?apikey=8LYjRmrxFnGAGVTSE4qQnc7XxhnO45t9`
+      )
+      .then((response) => {
+        setConditions(response.data[0]);
+      });
+  }, [cityInfo]);
 
-    return ( 
-        <>
-            {conditions && (
-                <main className="current-conditions-box">
-                    <p>Weather for {cityInfo.EnglishName}, {cityInfo.Country.EnglishName}:</p>
-                    <h3 className="city-country">
-                        {cityInfo.EnglishName} {cityInfo.Country.EnglishName}
-                    </h3>
-                    <div className="details">
-                        <h2 className="temperature-value">
-                            {Math.ceil(conditions.Temperature.Metric.Value)}
-                            <sup className="deg">&deg;{conditions.Temperature.Metric.Unit}</sup>
-                        </h2>
-                        {/* {conditions.IsDayTime === true ? <img className="weather-img" src={sun} alt="sun" /> : <img className="weather-img" src={moon} alt="moon" />} */}
-                        <p className="weather-text">{conditions.WeatherText}</p>
-                    </div>
-                </main>
-            )}
+  return (
+    <>
+      {conditions && (
+        <article className="current-conditions-box">
+          <p>
+            Weather for {cityInfo.EnglishName}, {cityInfo.Country.EnglishName}:
+          </p>
+          <h3 className="city-country">
+            {cityInfo.EnglishName} {cityInfo.Country.EnglishName}
+          </h3>
+          <div className="details">
+            <h2 className="temperature-value">
+              {Math.ceil(conditions.Temperature.Metric.Value)}
+              <sup className="deg">
+                &deg;{conditions.Temperature.Metric.Unit}
+              </sup>
+            </h2>
+            {/* {conditions.IsDayTime === true ? <img className="weather-img" src={sun} alt="sun" /> : <img className="weather-img" src={moon} alt="moon" />} */}
+            <p className="weather-text">{conditions.WeatherText}</p>
+          </div>
+        </article>
+      )}
+    </>
+  );
+};
 
-        </>
-     );
-}
- 
 export default Widget;
